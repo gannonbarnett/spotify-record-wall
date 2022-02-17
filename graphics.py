38845242b80cfae5ac1153291510037141aa3e0e
Gannon Barnett <barnettg@mit.edu>
2022-02-17 13:18:04 -0500

    Working with liked songs and playlists

---
+++ graphics.py
@@ -0,0 +1,49 @@
+import math
+import requests
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+from tqdm import tqdm
+
+BEBAS = "Bebas-Regular.ttf"
+
+def make_poster(visuals, outfile="out.png", font_name=BEBAS, default_font_size=30, num_w=None):
+    if not num_w:
+        num_w = math.ceil(math.sqrt(len(visuals)))
+
+    img_size = 300
+    caption_h_padding = 3
+    caption_h = default_font_size + caption_h_padding * 2
+    num_h = len(visuals) // num_w + 1 
+
+    canvas = Image.new('RGB', (num_w*img_size, num_h * img_size + num_h * caption_h))
+    draw = ImageDraw.Draw(canvas)
+    i = 0
+    with tqdm(total=len(visuals)) as pbar:
+        for row in range(num_h):
+            for col in range(num_w):
+                if i >= len(visuals) - 1:
+                    continue
+                # visual {albulm name, artist, img_url}
+                img_url = visuals[i][2]
+                r = requests.get(img_url)
+                img = Image.open(BytesIO(r.content))
+                pos = (col * img_size, row * (img_size + caption_h))
+                canvas.paste(img, pos)
+                try:
+                    albulm_name = visuals[i][0]
+                    font_size = default_font_size
+                    font = ImageFont.truetype(font_name, font_size)
+                    size = draw.textsize(albulm_name, font)
+                    while size[0] > img_size:
+                        font_size -= 1
+                        font = ImageFont.truetype(font_name, font_size)
+                        size = draw.textsize(albulm_name, font)
+                    draw.text((col * img_size + (img_size - size[0])/2, row * (img_size + caption_h) + img_size + caption_h_padding), visuals[i][0], font=font)
+                except Exception as e:
+                    print(e)
+                    pass 
+
+                i += 1
+                pbar.update(i)
+    print("Saving output...")
+    canvas.save(outfile)
---
+++ main.py
@@ -0,0 +1,83 @@
+from typing import Set
+import graphics
+from common import authenticate_user
+
+# https://developer.spotify.com/web-api/using-scopes/
+scope = 'user-library-read user-top-read playlist-read-private'
+
+# Return username, spotify
+def auth():
+    return authenticate_user('user-library-read user-top-read playlist-read-private')
+
+def curr_user_library_tracks(spotify):
+    tracks = []
+    total = 1
+    while len(tracks) < total:
+        tracks_response = spotify.current_user_saved_tracks(offset=len(tracks))
+        tracks.extend(tracks_response.get('items', []))
+        total = tracks_response.get('total')
+
+    if 'track' in tracks[0]:
+        return [track.get('track') for track in tracks]
+    else: 
+        return tracks
+    
+def curr_user_playlists(spotify):
+    resp = spotify.current_user_playlists()
+    playlists = resp['items']
+    while resp['next']:
+        resp = spotify.next(resp)
+        playlists.extend(resp['items'])
+    return playlists 
+
+def curr_user_playlist_tracks(spotify, playlists, target_name):
+    p = None
+    for item in playlists:
+        if item['name'] == target_name:
+            p = item
+            break
+    if not p:
+        print(f"Couldn't find playlist '{target_name}'")
+        names = [p['name'] for p in playlists]
+        print(names)
+
+    tracks = []
+    resp = spotify.playlist(p['id'])
+    tracks.extend(resp['tracks']['items']) 
+    while resp and 'next' in resp['tracks']:
+        resp = spotify.next(resp['tracks'])
+        if resp:
+            tracks.extend(resp['tracks']['items']) 
+
+    return [track.get('track') for track in tracks]
+
+def make_visual_configs(tracks):
+    print(tracks[0])
+    visuals = []
+    seen = set() 
+    for t in tracks:
+        name = t['name']
+        artist = t['artists'][0]['name']
+        img_url = t['album']['images'][1]['url']
+        if img_url not in seen:
+            seen.add(img_url)
+            visuals.append([name, artist, img_url])
+    return visuals
+
+def generate_liked_songs_poster(outfile):
+    _, spotify = auth()
+    tracks = curr_user_library_tracks(spotify)
+    visual_configs = make_visual_configs(tracks) 
+    graphics.make_poster(visual_configs, outfile=outfile)
+
+def generate_poster_from_playlist(outfile, playlist_name):
+    _, spotify = auth()
+    playlists = curr_user_playlists(spotify)
+    tracks = curr_user_playlist_tracks(spotify, playlists, playlist_name)
+    visual_configs = make_visual_configs(tracks) 
+    graphics.make_poster(visual_configs, outfile=outfile)
+
+if __name__ == '__main__':
+    # generate_liked_songs_poster("out.png")
+    # generate_poster_from_playlist('out.png', 'Your Top Songs 2021')
+
